da3c6c4c087bbc6cb2fdb39394b1b8a9b9d92072
crazymacer <mario_ct5@hotmail.com>
2018-08-11 12:26:46 -0500

    Improves

--- json.php
+++ json.php
@@ -1,51 +1,89 @@
 <?php
 header('Content-Type: application/json');
-/*
-$prep   = $this->conexion->prepare($sql);  
-$prep->execute();   
-$result = $prep->fetchAll(PDO::FETCH_ASSOC);
 
-$data   = [];
-foreach ($result as $key => $value) {
-    $InfoData=[]; 
-    foreach ($value as $key1 => $value1) {
-        $InfoData[$key1] = $value1;
-    }
-    $data[] = $InfoData;
-}
 
-$json_data = [
-    "data"   => $data   
-];
-
-echo json_encode($json_data); 
-
-*/
-
-    $data   = [];
+if (!empty($_POST["ProjectId"])) 
+{
     
-    $InfoData1=[]; 
-    $InfoData1["Id"] = "1";
-    $InfoData1["Dni"] = "54545465";
-    $InfoData1["AssigmentDate"] = "12/08/2018";
-    $InfoData1["FirstName"] = "Jhon";
-    $InfoData1["LastName"] = "Doe";
-    $InfoData1["JobTitle"] = "Operador";
-    $InfoData1["Observation"] = "Ninguna";
-    $data[] = $InfoData1;
+    //If para saber si existe o no ese registro
+    if ($_POST["ProjectId"]!="") 
+    {
+        $data   = [];
 
-    $InfoData2=[]; 
-    $InfoData2["Id"] = "2";
-    $InfoData2["Dni"] = "65451565";
-    $InfoData2["AssigmentDate"] = "20/08/2018";
-    $InfoData2["FirstName"] = "Jane";
-    $InfoData2["LastName"] = "Smith";
-    $InfoData2["JobTitle"] = "Auxiliar";
-    $InfoData2["Observation"] = "Ninguna";
-    $data[] = $InfoData2;
+        for ($i=1; $i < 50; $i++)
+        { 
+            
+            $InfoData1=[]; 
+            $InfoData1["Id"] = $i;
+            $InfoData1["Dni"] = "54545465";
+            $InfoData1["AssigmentDate"] = "12/08/2018";
+            $InfoData1["FirstName"] = "Jhon";
+            $InfoData1["LastName"] = "Doe";
+            $InfoData1["JobTitle"] = "Operador";
+            $InfoData1["Observation"] = "Ninguna";
+            $data[] = $InfoData1;
+            
+        }
+    
 
-$json_data = [
-    "data"   => $data   
-];
+        $json_data = [
+            "data"   => $data   
+        ];
 
-echo json_encode($json_data); 
+        echo json_encode($json_data); 
+    }
+    else
+    {
+        
+        $InfoData1=[]; 
+        $InfoData1["sEcho"] = "1";
+        $InfoData1["iTotalRecords"] = "0";
+        $InfoData1["iTotalDisplayRecords"] = "0";
+
+        $blank   = [];
+
+        $InfoData1["aaData"] =  $blank;
+
+     
+
+        $json_data = [
+            "data"   => $InfoData1   
+        ];
+
+        echo json_encode($InfoData1); 
+
+    }
+
+}
+else
+    {
+       
+
+        $InfoData1=[]; 
+        $InfoData1["sEcho"] = "1";
+        $InfoData1["iTotalRecords"] = "0";
+        $InfoData1["iTotalDisplayRecords"] = "0";
+
+        $blank   = [];
+
+        $InfoData1["aaData"] =  $blank;
+
+     
+
+        $json_data = [
+            "data"   => $InfoData1   
+        ];
+
+        echo json_encode($InfoData1); 
+
+
+    }
+
+
+
+
+
+
+
+
+
